baf503e8a743f6e4afb7f4eee79b3804e1dbc3a5
Scott Ian <sim2487@gmail.com>
2020-04-01 20:58:54 -0700

    Add Chats list component.

---
+++ src/chatsList/ChatsComponent.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react'
+import { List } from '@material-ui/core';
+import { ListItem } from '@material-ui/core';
+import { ListItemText } from '@material-ui/core';
+import { ListItemAvatar } from '@material-ui/core';
+import { Avatar } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { Divider } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
+
+
+class ChatsComponent extends Component {
+  
+  
+  
+  render() {
+    const { classes } = this.props;
+    console.log(classes);
+    
+    if (this.props.chats.length > 0) {
+      console.log('pos');
+      
+      return (
+        <main className={classes.root}>
+          <Button
+            variant='contained'
+            fullWidth
+            color='secondary'
+            className={classes.newChatBtn}
+            onClick={this.newChat}
+          >New Message</Button>
+          <List>
+            {
+              this.props.chats.map((_chat, _index) => {
+                return(
+                  <div key={_index}>
+                    <ListItem 
+                      onClick={() => this.selectChat(_index)}
+                      className={classes.listItem}
+                      selected={this.props.selectedChatIndex === _index}
+                      alignItems='flex-start' 
+                    >
+                      <ListItemAvatar>
+                        <Avatar>
+                          {_chat.users.filter(_user => _user !== this.props.userEmail)[0].split('')[0].toUpperCase()}
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText 
+                        primary={_chat.users.filter(_user => _user !== this.props.userEmail)[0]}
+                        secondary={
+                          <React.Fragment>
+                            <Typography component='span' color='textPrimary'>
+                              {
+                                _chat.messages[_chat.messages.length - 1].message.substring(0, 30)
+                              }
+                            </Typography>
+                          </React.Fragment>
+                        }
+                      >
+                      </ListItemText>
+                    </ListItem>
+                    <Divider></Divider>
+                  </div>
+                )
+              })
+            }
+          </List>
+        </main>
+      )
+    } else {
+      return(
+        <div className={classes.root}>
+          <Button 
+            variant="contained" 
+            fullWidth 
+            color='secondary' 
+            onClick={this.newChat} 
+            className={classes.newChatBtn}
+          >New Message
+          </Button>
+          <List></List>
+        </div>
+      );
+    }
+    
+  }
+  
+  newChat = () => {
+    console.log('New Chat button clicked');
+  }
+  
+  selectChat = index => {
+    console.log('selectChat', index);
+    
+  }
+}
+
+export default withStyles(styles)(ChatsComponent);
--- src/dashboard/DashboardComponent.js
+++ src/dashboard/DashboardComponent.js
@@ -1,11 +1,68 @@
 import React, { Component } from 'react'
-
+import ChatsComponent from '../chatsList/ChatsComponent';
+const firebase = require('firebase');
 export default class DashboardComponent extends Component {
+  constructor() {
+    super();
+  
+    this.state = {
+      selectedChat: null,
+      newChatFormVisible: false,
+      email: null,
+      chats: []
+    };
+  }
+  
   render() {
+    
     return (
       <div>
-        Hello from dashboard.
+        <ChatsComponent 
+          history={this.props.history}
+          onNewChatClickFn={this.onNewChatClick}
+          onSelectChatFn={this.onSelectChat}
+          chats={this.state.chats}
+          userEmail={this.state.email}
+          selectedChatIndex={this.state.selectedChat}
+        ></ChatsComponent>
       </div>
+      
     )
   }
+  
+  onSelectChat = chatIndex => {
+    console.log('onSelectChat', chatIndex);
+    
+  } 
+  
+  onNewChatClick = () => {
+    console.log('onNewChatButtonClick');
+    this.setState({
+      newChatFormVisible: true,
+      selectedChat: null,
+    });
+  }
+  
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(async _usr => {
+      if(!_usr) {
+        this.props.history.push('/login');
+      } else {
+        await firebase
+          .firestore()
+          .collection('chats')
+          .where('users', 'array-contains', _usr.email)
+          .onSnapshot(async res => {
+            const chats = res.docs.map(_doc => _doc.data());
+            await this.setState({
+              email: _usr.email,
+              chats
+            });
+            console.log('onsnapshot', this.state);
+            
+          })
+      }
+    })
+  }
+  
 }
